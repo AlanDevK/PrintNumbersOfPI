2508baeeba14b7c041fe248ff6f939c1e0b7b78c
Alan <kazutodeveson@gmail.com>
2023-07-22 14:05:15 +0700

    Added project files to repository

---
+++ LearningC#/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("How many numbers of PI you want to print? ");
+int n = int.Parse(Console.ReadLine());
+string process = Convert.ToString((Math.Round(Math.PI, n) * Math.Pow(10,n))/10);
+string result = process.Substring(0, process.Length - 2);
+Console.WriteLine($"Value of PI: {result}");
